2ea9586f0461ebb50366b305bed5c618e0e9ffae
Albert Cerfeda <cerfeda.albert@gmail.com>
2022-11-09 22:22:58 +0100

    Implemented bench_metis

--- Assignment3/src/Tools/bench_metis.jl
+++ Assignment3/src/Tools/bench_metis.jl
@@ -17,4 +17,46 @@ function benchmark_metis()
     #       b) Perform direct k-way partitioning of the graphs in 16 and 32 subsets.
 
     #   3.  Visualize the results for 16 and 32 partitions.
+
+    #   List the meshes to compare
+    meshes = ["commanche_dual" "skirt"]
+
+    #   Init result array
+    pAll = Array{Any}(undef, 4, length(meshes)+1)
+
+    pAll[1,1] = "16-recursive bisection"
+    pAll[2,1] = "16-way direct bisection"
+    pAll[3,1] = "32-recursive bisection"
+    pAll[4,1] = "32-way direct bisection"
+    
+    #   Loop through meshes
+    for (i, mesh) in enumerate(meshes)
+        println("\n\n",mesh,"\n===\n")
+        #   Define path to mat file
+        path = joinpath(dirname(@__DIR__),"Meshes","2D",mesh*".mat");
+
+        #   Read data
+        A, coords = read_mat_graph(path);
+        #   1st row
+        p = metis_part(A, 16, :RECURSIVE)
+        pAll[1,i+1] = cut = count_edge_cut(A, p)
+        GLMakie.save("out/plots/metis/metis-$mesh-recursive-p16-cut:$cut.png",draw_graph(A, coords, p))
+
+        p = metis_part(A, 16, :KWAY)
+        pAll[2,i+1] = cut = count_edge_cut(A, p)
+        GLMakie.save("out/plots/metis/metis-$mesh-kway-p16-cut:$cut.png",draw_graph(A, coords, p))
+
+        p = metis_part(A, 32, :RECURSIVE)
+        pAll[3,i+1] = cut = count_edge_cut(A, p)
+        GLMakie.save("out/plots/metis/metis-$mesh-recursive-p16-cut:$cut.png",draw_graph(A, coords, p))
+
+        p = metis_part(A, 32, :KWAY)
+        pAll[4,i+1] = cut = count_edge_cut(A, p)
+        GLMakie.save("out/plots/metis/metis-$mesh-kway-p16-cut:$cut.png",draw_graph(A, coords, p))
+
+    end
+
+    #   Print result table
+    header =["Partitions","Helicopter","Skirt"]
+    pretty_table(pAll; header = header, crop = :none, header_crayon = crayon"bold cyan")
 end
